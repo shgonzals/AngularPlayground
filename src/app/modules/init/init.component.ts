b47296a83cbd0d8cd4bee1e6da739e08ff519235
shgonzals <sh.gonzal.s@gmail.com>
2024-02-08 11:10:21 +0100

    improve logo and styles

--- src/app/modules/init/init.component.ts
+++ src/app/modules/init/init.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,7 +6,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './init.component.html',
-  styleUrl: './init.component.scss'
+  styleUrl: './init.component.scss',
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 1 }),
+        animate('800ms', style({ opacity: 1 })),
+      ]),
+    ]),
+    trigger('slideAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(-50%)' }),
+        animate('900ms ease-in-out', style({ transform: 'translateX(0%)'})),
+      ]),
+    ]),
+  ]
 })
 export class InitComponent {
 
