6d448fab3ed41d8f7155eb75c62a90413f556e48
shgonzals <sh.gonzal.s@gmail.com>
2024-02-08 17:18:33 +0100

    add @vercel/analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './modules/sidebar/sidebar.component';
 import { ListComponent } from './components/list/list.component';
-
+import { inject } from '@vercel/analytics';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,6 +15,10 @@ import { ListComponent } from './components/list/list.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    inject();
+  }
 
 }
